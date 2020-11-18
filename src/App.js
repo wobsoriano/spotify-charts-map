b11aa49a2a83255823b10645e51ccb18eb3d10a9
Robert Anthonie Soriano <sorianorobertc@gmail.com>
2020-11-18 14:57:40 +0800

    fix tooltip issue

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import { useState } from 'react';
 import ReactTooltip from 'react-tooltip';
-import { QueryCache, ReactQueryCacheProvider } from 'react-query';
+import { QueryCache, ReactQueryCacheProvider, useQuery } from 'react-query';
 
 import MapChart from './MapChart';
+import Loader from './Loader';
 import './App.css';
 
 const queryCache = new QueryCache();
 
 export default function App() {
   const [content, setContent] = useState('');
+  const { isLoading, data } = useQuery('repoData', () =>
+    fetch(
+      'https://raw.githubusercontent.com/wobsoriano/spotify-charts-map/master/data/spotifycharts.json'
+    ).then((res) => res.json())
+  );
 
   return (
     <ReactQueryCacheProvider queryCache={queryCache}>
       <div className="App">
         <h1 className="Title">Spotify Top Track by Region</h1>
         <div className="Subtitle">Hover on a country to see the most streamed track of the day</div>
-        <MapChart setTooltipContent={setContent} />
+
+        {isLoading ? <Loader /> : <MapChart countries={data} setTooltipContent={setContent} />}
+
         <div className="Footer-text">
           Powered by{' '}
           <a
--- src/MapChart.js
+++ src/MapChart.js
@@ -1,9 +1,7 @@
-import React, { useEffect, memo } from 'react';
-import { useQuery } from 'react-query';
+import React, { memo, useEffect } from 'react';
 import { ZoomableGroup, ComposableMap, Geographies, Geography, Graticule } from 'react-simple-maps';
 import ReactTooltip from 'react-tooltip';
 
-import Loader from './Loader';
 import './MapChart.css';
 
 const geoUrl =
@@ -11,14 +9,10 @@ const geoUrl =
 
 const withCommas = (num) => Intl.NumberFormat().format(num);
 
-const MapChart = ({ setTooltipContent }) => {
-  const { isLoading, data } = useQuery('repoData', () =>
-    fetch(
-      'https://raw.githubusercontent.com/wobsoriano/spotify-charts-map/master/data/spotifycharts.json'
-    ).then((res) => res.json())
-  );
-
-  if (isLoading) return <Loader />;
+const MapChart = ({ setTooltipContent, countries }) => {
+  useEffect(() => {
+    ReactTooltip.rebuild();
+  }, []);
 
   return (
     <>
@@ -28,7 +22,7 @@ const MapChart = ({ setTooltipContent }) => {
           <Geographies geography={geoUrl}>
             {({ geographies }) =>
               geographies.map((geo) => {
-                const exists = Object.keys(data).find((i) => i === geo.properties.ISO_A2);
+                const exists = Object.keys(countries).find((i) => i === geo.properties.ISO_A2);
                 return (
                   <Geography
                     key={geo.rsmKey}
@@ -38,7 +32,7 @@ const MapChart = ({ setTooltipContent }) => {
                       const { NAME, ISO_A2 } = geo.properties;
                       if (exists) {
                         ReactTooltip.rebuild();
-                        const countryData = data[ISO_A2];
+                        const countryData = countries[ISO_A2];
                         setTooltipContent(
                           `${NAME}:<br />Track: <span style="font-weight: bold;">${
                             countryData.trackName
@@ -55,7 +49,7 @@ const MapChart = ({ setTooltipContent }) => {
                     }}
                     onClick={() => {
                       if (exists) {
-                        const { url } = data[geo.properties.ISO_A2];
+                        const { url } = countries[geo.properties.ISO_A2];
                         const win = window.open(url, '_blank');
                         win.focus();
                       }
